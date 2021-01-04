0680e2fa46b1264fbb60de763fd56118ee14295b
Zoe <heejeong.cho@stryx.co.kr>
2021-01-04 16:11:37 +0900

    delete dotenv

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,14 +8,14 @@ export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   // because Nuxt.js doesn’t have a default index.html
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'nuxt-basic',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_name_description || '',
+        content: '',
       },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
